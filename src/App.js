d9b8a30509bc24f73d3e886c2b0b56beedc66d7b
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-13 00:31:05 +0100

    Add handle add  item to cart feature

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import { Products, NavBar } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    console.log(item);
+    setCart(item.cart);
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
+
+  console.log(cart);
 
   return (
     <div>
-      <NavBar />
-      <Products products={products} />
+      <NavBar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
